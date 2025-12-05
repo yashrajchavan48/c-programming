c20ab607b15ed2dab2946a26b6f96c25dbe7fc83
yashrajchavan48 <yashraj4048@eng.rizvi.edu.in>
2025-12-05 15:32:07 +0530

    exp 2-1 added

---
+++ 2-1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ /*yashraj chavan  First Year- D - Computer
+Uin- 251P096*/
+
+int main() {
+int num;
+printf("Enter a Number:");
+scanf("%d",&num);
+
+if(num%2 == 0){
+    printf("Even Number");
+}
+else{
+    printf("Odd Number");
+}
+    return 0;
+}
