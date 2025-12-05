08485cdb0dacef8a912aa999d8371aeb1c36a52e
yashrajchavan48 <yashraj4048@eng.rizvi.edu.in>
2025-12-05 15:23:51 +0530

    exp 1-1 added

---
+++ 1-1.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+
+int main(){
+    /* yashraj chavan 
+    18
+    First Year
+    D
+    251P096
+    */
+
+printf("Name: yashraj chavan\n");
+printf("Age: 18\n");
+printf("Class: First Year\n");
+printf("Division: D\n");
+printf("UIN: 251P096\n");
+
+return 0;
+}
